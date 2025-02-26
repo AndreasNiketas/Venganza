b2df23ecb291dba3c762c4889d2090b09063207a
Andres Pabuena <andrespabuena10@gmail.com>
2025-02-25 20:53:41 -0500

    1.0

---
+++ script.js
@@ -0,0 +1,7 @@
+function openModal(src) {
+    document.getElementById("modal-img").src = src;
+    document.getElementById("modal").style.display = "flex";
+}
+function closeModal() {
+    document.getElementById("modal").style.display = "none";
+}
