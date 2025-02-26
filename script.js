54e5f549684b89866cd1c410c13f002b130bce48
Andres Pabuena <andrespabuena10@gmail.com>
2025-02-25 21:48:46 -0500

    2.1

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+// script.js
 function openModal(src) {
     document.getElementById("modal-img").src = src;
     document.getElementById("modal").style.display = "flex";
 }
+
 function closeModal() {
     document.getElementById("modal").style.display = "none";
+}
+
+function showGallery(galleryNumber) {
+    document.getElementById("gallery1").style.display = galleryNumber === 1 ? "grid" : "none";
+    document.getElementById("gallery2").style.display = galleryNumber === 2 ? "grid" : "none";
 }
